4fe1855fea385321a75e8a28c8461f78c5d82f9f
Sanika Surve <sanika.psurve@gmail.com>
2026-02-08 20:17:42 +0530

    added cron

--- index.mjs
+++ index.mjs
@@ -1,7 +1,8 @@
 import Slack from '@slack/bolt'
 import dotenv from 'dotenv'
+import cron from 'node-cron'
 dotenv.config()
-
+async function slackCron(){
 const app = new Slack.App({
     signingSecret: process.env.SLACK_SIGNINGSECRET,
     token: process.env.SLACK_TOKEN
@@ -11,5 +12,12 @@ await app.client.chat.postMessage({
     channel: process.env.CHANNEL,
     text: `Hello <@U0ADMQZ7DSS>`
 })
+}
+
+
+cron.schedule('30 10 * * 1-6',()=>{
+    slackCron()
 console.log("message sent Successfully!");
+})
+
 
